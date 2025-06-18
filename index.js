5692b4de0a474cf9d3c52129a438865aa9ef0e3d
EstilodoGobetti <rodrigogobettimiranda@gmail.com>
2025-06-18 13:04:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 touch index.js
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+// Usa o token que você colocou no arquivo .env
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+// Trecho TEMPORÁRIO só para descobrir o chat ID:
+bot.on('message', (msg) => {
+  console.log(JSON.stringify(msg, null, 2));
+});
