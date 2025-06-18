ed11db3e61ee4559232c46f45b565225426aa4c1
EstilodoGobetti <rodrigogobettimiranda@gmail.com>
2025-06-18 13:18:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
 
-bot.sendMessage(process.env.CHAT_ID, "🚀 Bot iniciado com sucesso e pronto para enviar promoções!");
+bot.sendMessage(process.env.CHAT_ID, "🚀 Bot iniciado com sucesso e pronto para enviar promoções!")
+  .then(() => {
+    console.log("Mensagem enviada com sucesso!");
+    process.exit(0); // Encerra o programa após enviar
+  })
+  .catch((error) => {
+    console.error("Erro ao enviar mensagem:", error);
+    process.exit(1);
+  });
